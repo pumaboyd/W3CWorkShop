2863dc2ff8ff687fe03309438735482f3954dc51
pumaboyd <pumaboyd@163.com>
2013-07-10 21:04:36 +0800

    git file upload
    git file upload

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
-phpinfo();
+#phpinfo();
+define('BR', '<br/>');
+
+echo __FILE__;
+echo BR;
+
+echo dirname(__FILE__).'';
+echo BR;
+
+$basedir = dirname(dirname(__FILE__));     
+echo $basedir;
+echo BR;
+
+
+
+if (defined('PROJECT_NAME')) {
+	echo '存在';
+	echo BR;
+} else {
+	echo "不存在";
+	echo BR;
+}
+
+$array = array(
+         "a",
+         "b",
+    6 => "c",
+         "d",
+);
+var_dump($array);
+
 ?>
